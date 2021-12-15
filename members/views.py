2e4c00b4d2fc0a6dd9c207f93b94dfeaea8479bd
YONGJAISONG <96100034+YONGJAISONG@users.noreply.github.com>
2021-12-15 17:16:46 +0900

    멤버 url 생성

--- members/views.py
+++ members/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+from .models import members
+
+def index(request):
+    member = members.objects.all()
+
+    return render(
+        request,
+        'members/index.html',
+        {
+            'member' : member,
+        }
+    )
