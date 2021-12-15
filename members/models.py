28583be551574975197b2ac052e7a295d785ea60
YONGJAISONG <96100034+YONGJAISONG@users.noreply.github.com>
2021-12-15 16:47:42 +0900

    members app에 정보 입력
    - 멤버 정보 입력
    - 제목이 멤버 이름이 되도록 수정

--- members/models.py
+++ members/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class members(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField(max_length=5)
+    birth = models.DateTimeField()
+    email = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'[{self.pk}] {self.name}'
